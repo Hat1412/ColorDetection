58e99b1e6efc3b572ca67b165a17d3fb92894955
Hat1412 <hari8haran8@gmail.com>
2022-04-16 14:42:14 +0530

    Edited file

--- ColorDetection.py
+++ ColorDetection.py
@@ -1,89 +1,42 @@
 from tkinter import *
-from pynput.mouse import Listener
 from pyautogui import pixel
-from tkinter import messagebox
-import cv2
-from PIL import Image, ImageTk
+from pynput.mouse import Listener
 
 root = Tk()
-root.config(bg="#EFE4B0")
-H,W = 300,1000
-root.attributes("-topmost", True)
-root.geometry(f"{W}x{H}")
-color = StringVar()
-counter = 1
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=BOTH)
+root.config(bg = "#99d9ea")
+root.attributes("-topmost",True)
 
-history = Listbox(root, font=("Times New Roman", 20, "bold"), bg="#A349A4", fg="white")
-history.pack(side="right", fill=Y)
-history.insert(0, "History")
-history.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=history.yview)
+RGB = IntVar()
+HEX = IntVar()
+switch = False
 
-Entry(root, textvariable=color, font=("Times New Roman", 20, "bold"),
-bg="#99D9EA").pack(padx=20, side="top")
+def on_click(x,y,_,pressed):
+    global switch
+    data = []
+    if RGB.get():
+        data.append(pixel(x,y))
+    if HEX.get():
+        color = '#%x%x%x' % pixel(x,y)
+        data.append(color)
 
-Button(root,text="Copy Color",bg="#99D9EA",
-    font=("Times New Roman", 20, "bold"),
-    command = lambda: root.clipboard_append(color.get())).pack(side="top")
-
-Label(root,text="Click Anywhere To View Color",
-font=("Times New Roman", 20, "bold"),bg="#EFE4B0").pack(padx=30, pady=30)
-
-fr = Frame(root, bg="black", bd=2)
-fr.pack()
+    if switch:
+        root.clipboard_clear()
+        root.clipboard_append(data)
+        switch = False        
 
 
-def on_click(x, y, _, pressed):
-    global history, counter
-    a = pixel(x, y)
-    if pressed:
-        if a[0] not in range(root.winfo_x(),root.winfo_x() + W+100) or a[1] not in range(root.winfo_y(),root.winfo_y() + H+50):
-   
-            color_code = f"""{a} {'#%02x%02x%02x' % (a[0], a[1], a[2])} """
-            color.set(color_code)
-            history.insert(counter, color_code)
-            if counter > 0:
-                history.selection_clear(counter - 1)
-            history.selection_set(counter)
-            
-            counter += 1
+def get_color():
+    global switch
+    switch = True
 
+Button(root,text = "Get Color",fg = "#FFFFFF",command = get_color,bg = "#22b14c",font = ("Times New Roman",15)).pack(padx = 10,pady = 10)
+Checkbutton(root, text="RGB VALUE", variable=RGB,fg = "#ff7f27", onvalue=1, offvalue=0,
+bg = "#88015c",font = ("Times New Roman",20)).pack(side = "left",padx = 10,pady = 10)
 
-def tk_webcam():
-    global label, cap, chk
-    cap = cv2.VideoCapture(0)
-    label = Label(root)
-    label.pack(side="bottom")
-    messagebox.showinfo("Webacam Started", "Press Q to stop webcam !")
-    show_frames()
-
-
-Button(fr,text="Turn on Webcam",bg="#7092BE",
-    font=("Times New Roman", 20, "bold"),
-    command=tk_webcam).grid(row=0, column=0)
-
-Button(fr,text="Clear History", bg="#7092BE",
-    font=("Times New Roman", 20, "bold"),
-    command=lambda: history.delete(1, END)).grid(row=0, column=1)
-
-
-def stop_webcam(_):
-    cap.release
-    label.pack_forget()
-
-
-def show_frames():
-    cv2image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    label.imgtk = imgtk
-    label.configure(image=imgtk)
-    label.after(20, show_frames)
-
+Checkbutton(root, text="HEX VALUE", variable=HEX,fg = "#ff7f27", onvalue=1, offvalue=0,state = "active",
+bg = "#88015c",font = ("Times New Roman",20)).pack(side = "left",padx = 10,pady = 10)
 
 with Listener(on_click=on_click) as listener:
-    root.bind("q", stop_webcam)
     root.mainloop()
     listener.join()
+
